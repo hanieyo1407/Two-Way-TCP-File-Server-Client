9d42fa54e90717334855b7668ec92ef8191c8264
hanieyo1407 <ymaluwa@gmail.com>
2025-06-03 09:31:30 +0200

    Handling file reception for the server from the client logic!

--- app.py
+++ app.py
@@ -22,7 +22,46 @@ def start_server_receive_file(host, port, save_directory = "received_files"):
             if not os.path.exists(save_directory):
                 os.makedirs(save_directory)
                 print(f"Created directory: {save_directory}")
-            print("Connection closed")
+
+
+            try:
+                filename_len_bytes = conn.recv(4)
+                if not filename_len_bytes:
+                    print("Client disconnected before sending filename length.")
+
+                filename_len_bytes = struct.unpack('!I', filename_len_bytes)[0]
+
+                filename = conn.recv(filename_len_bytes).decode('utf-8')
+                save_path = os.path.join(save_directory, filename)
+                print(f"Receiving file: {filename} and saving to {save_path}")
+
+
+                file_size_bytes = conn.recv(8)
+                if not file_size_bytes:
+                    print("Client disconnected before sending file size.")
+                    return
+                file_size = struct.unpack('!Q', file_size_bytes)[0]
+                print(f"Expected file size : {file_size} bytes.")
+
+                received_bytes = 0
+                with open(save_path, 'wb') as f:
+                    while received_bytes < file_size:
+                        bytes_to_read = min(BUFFER_SIZE, file_size - received_bytes)
+                        data = conn.recv(bytes_to_read)
+                        if not data:
+                            print("Client disconnected prematurely.")
+                            break
+                        f.write(data)
+                        received_bytes += len(data)
+                        print(f"Recceived {received_bytes}/{file_size} bytes ({((received_bytes/file_size)*100):.2f}%)",end='\r')
+                print(f"\n successfully received '{filename}'. Total bytes: {received_bytes}")
+
+            except ConnectionResetError:
+                print("Client unexpectedly disconnected🥺!")
+            except Exception as e:
+                print(f"An Error occured during file reception: {e}")
+            finally:
+                print("Connection closed")
 
 
 
