a77a272db9bc3bf3961f08918a041d28b811b636
hanieyo1407 <ymaluwa@gmail.com>
2025-06-03 09:28:25 +0200

    Creating the logic for client requesting for connection to server!

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import socket
 import os
+import struct
 
 HOST = '127.0.0.1'
 PORT = 65432
@@ -21,9 +22,20 @@ def start_server_receive_file(host, port, save_directory = "received_files"):
             if not os.path.exists(save_directory):
                 os.makedirs(save_directory)
                 print(f"Created directory: {save_directory}")
-
-            print("Ready to receive file..")
             print("Connection closed")
+
+
+
+def start_client_send_file(host, port, file_path_to_send = "tst_file.txt"):
+    print(f"Client attempting to connect to {host}:{port}")
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((host, port))
+        print(f"connected to server at {host}:{port}")
+
+        print(f"Ready to send file: {file_path_to_send}")
+        print("Connection closed.")
+
 
 if __name__ == "__main__":
     if not os.path.exists("received_files"):
